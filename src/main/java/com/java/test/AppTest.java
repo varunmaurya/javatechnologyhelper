f379b15b4cf90d5f18118f5f1b7ba7ad1c340060
varun <varun@VARUN-PC>
2016-06-02 23:26:39 -0500

    Added AppTest.java

---
+++ src/main/java/com/java/test/AppTest.java
@@ -0,0 +1,10 @@
+package com.java.test;
+
+import com.java.file.reader.FileReader;
+
+public class AppTest {
+	public static void main(String[] args) {
+		FileReader reader = new FileReader();
+		reader.read();
+	}
+}
